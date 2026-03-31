9fc969c66cff4ac02b10fcb4764f2fea5fd1d002
Dornori <135155125+dornori@users.noreply.github.com>
2026-03-31 11:27:19 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,24 @@
+const SITE_CONFIG = {
+    appearance: {
+        icyLemon: "#F5F29B",
+        bgDark: "#050505",
+        bannerStickyOffset: 0.35,
+        navFontWeight: "600"
+    },
+    navigation: [
+        { label: "About", link: "about.html", type: "standard", enabled: false },
+        { label: "Contact", link: "contact.html", type: "standard", enabled: false },
+        { label: "Newsletter", link: "#newsletter", type: "button", enabled: true }
+    ],
+    socials: [
+        { id: 'ig',  user: "dornori.info", base: 'https://instagram.com/' },
+        { id: 'x',   user: "dornori_info", base: 'https://x.com/' },
+        { id: 'yt',  user: "dornori_info", base: 'https://youtube.com/@' },
+        { id: 'fb',  user: "profile.php?id=61585253280713",  base: 'https://facebook.com/' },
+        { id: 'web', user: "dornori.com",   base: 'https://' }
+    ],
+    formspree_id: "xnjopbbb",
+    turnstile_sitekey: "0x4AAAAAACxsga5y-bJ_qkzC"
+};
+
+export default SITE_CONFIG;
