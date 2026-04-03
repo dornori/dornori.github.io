f7f736500e7a504164ea49253d7e6a2696d2831a
Dornori <135155125+dornori@users.noreply.github.com>
2026-04-03 11:07:27 +0200

    Update nav-loader.js

--- js/nav-loader.js
+++ js/nav-loader.js
@@ -5,6 +5,11 @@ import SITE_CONFIG from './config.js';
  * Handles the top-right menu. 
  * Updated to handle the 'Newsletter' scroll-to-focus behavior.
  */
+
+function toggleTheme() {
+    document.body.classList.toggle("light-theme");
+}
+
 export function initNavigation() {
     const nav = document.querySelector('.top-nav');
     if (!nav) return;
