862a4b9a261704885e0dcb1dee6c81fec63d5971
Dornori <135155125+dornori@users.noreply.github.com>
2026-05-08 18:37:35 +0200

    Create config.js

---
+++ sandbox/content/en/config.js
@@ -0,0 +1,12 @@
+// config.js
+const CONFIG = {
+    turnstile: {
+        sitekey: "1x00000000000000000000AA"
+    },
+    queue: {
+        endpoint: "https://edge-form-handler-api.dornori-info.workers.dev"
+    },
+    formJsonPath: "lang/en/form.json"     // ← JSON path is here
+};
+
+window.CONFIG = CONFIG;
