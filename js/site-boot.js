017f1e31c340c51cda0bff1d01a3985366661be8
Dornori <135155125+dornori@users.noreply.github.com>
2026-05-11 16:12:08 +0200

    Update site-boot.js

--- js/site-boot.js
+++ js/site-boot.js
@@ -1,5 +1,5 @@
 import { loadScript } from './utils/script-loader.js';
-import ENV_CONFIG from '../env-config.js';
+import ENV_CONFIG from './env-config.js';
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').catch(err => {
