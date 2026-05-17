4ce4f68a40d5e0628a448b8e4ba773644f456772
Dornori <135155125+dornori@users.noreply.github.com>
2026-05-17 10:23:56 +0200

    cart update

--- js/page-init.js
+++ js/page-init.js
@@ -2,6 +2,7 @@
  * page-init.js
  * Tracks whether the page is scrolled to the top and toggles the
  * 'at-top' class on <html> accordingly (used to hide the scrollbar at top).
+ * Also tracks last visited shop page for cart navigation.
  */
 (function () {
     var html = document.documentElement;
@@ -24,4 +25,19 @@
     } else {
         initScrollListener();
     }
+
+    // Track last visited shop page for cart navigation
+    (function() {
+        var pageSlug = window.__PAGE_SLUG__;
+        var shopPages = ['shop', 'kit', 'parts', 'product'];
+        
+        if (pageSlug && shopPages.indexOf(pageSlug) !== -1) {
+            // Save the current page URL as the last visited shop page
+            try {
+                localStorage.setItem('webshop_last_page', window.location.href);
+            } catch (e) {
+                // localStorage not available
+            }
+        }
+    })();
 })();
