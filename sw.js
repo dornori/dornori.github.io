58d3087dfc0dfa18b825880f5e0996d77bb929d0
Dornori <135155125+dornori@users.noreply.github.com>
2026-05-18 00:54:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -36,7 +36,8 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       fetch(event.request)
         .then(response => {
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
           return response;
         })
         .catch(() => caches.match(event.request))
@@ -46,7 +47,8 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(response => {
         return response || fetch(event.request).then(fetchResponse => {
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, fetchResponse.clone()));
+          const clone = fetchResponse.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
           return fetchResponse;
         });
       })
