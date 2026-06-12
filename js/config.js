973b0103dfc4a0770da11b260c6d0061c723f23b
Dornori <135155125+dornori@users.noreply.github.com>
2026-06-12 21:48:40 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -152,6 +152,7 @@ const CONFIG = {
         files:               { file: 'files.html'             },
         'mission-statement': { file: 'mission-statement.html' },
         about:               { file: 'about-us.html'          },
+        python:              { file: 'files/python.html'      },
         terms:               { file: 'terms.html'             },
         privacy:             { file: 'privacy.html'           },
         children:            { file: 'children.html'          },
