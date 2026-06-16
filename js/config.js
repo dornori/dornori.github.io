b9d1999917af7e7c70bb874a3d8c94701e700e27
Dornori <135155125+dornori@users.noreply.github.com>
2026-06-16 16:07:32 +0200

    config

--- js/config.js
+++ js/config.js
@@ -98,7 +98,7 @@ const CONFIG = {
     payment: {
         activeProcessor: "paypal",
         paypal: {
-            clientId:   "AaYwnfwpOiekk9jVlaqW",
+            clientId:   "AaYwnfwpOiekk9jVlaqWJsTKNknCYNWEM6X1-5kav2sNq2t988oBkyr_MGwonIDDKxnTET4VorHpNEnN",
             currency:   "EUR",
             intent:     "capture",
             returnPath: "/success.html",
