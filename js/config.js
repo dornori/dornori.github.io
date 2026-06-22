44bdf28c220d35e2803a7e3c2d59bbfadc5ff9bf
Dornori <135155125+dornori@users.noreply.github.com>
2026-06-22 23:39:56 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -134,8 +134,8 @@ const CONFIG = {
         { slug: 'kit',    icon: 'building-kit-icon-200x200.svg',    type: 'standard', enabled: true  },
         { slug: 'parts',  icon: '3d-printer-icon-200x200.svg',      type: 'standard', enabled: true  },
         { slug: 'files',  icon: '3d-file-icon-200x200.svg',         type: 'standard', enabled: true,  children: [
+            { slug: 'files', label: 'Files', enabled: true },
             { slug: 'python', label: 'Python', enabled: true },
-            { slug: 'gallery', label: 'Gallery', enabled: true },
         ] },
         { slug: 'shop',   icon: 'shop-icon-200x200.svg',            type: 'standard', enabled: true  },
     ],
